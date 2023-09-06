f4dfd4fbeb3cd364240d4bfb9e04c9205ecf7771
valerieehuynh <vlhuynh2@uci.edu>
2023-09-05 19:08:07 -0700

    created starter interface

---
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './WeatherApp.css'
+import search_icon from '../Assets/search.png';
+import clear_icon from '../Assets/clear.png';
+import cloud_icon from '../Assets/cloud.png';
+import drizzle_icon from '../Assets/drizzle.png';
+import rain_icon from '../Assets/rain.png';
+import snow_icon from '../Assets/snow.png';
+import wind_icon from '../Assets/wind.png';
+import humid_icon from '../Assets/humidity.png';
+
+
+const WeatherApp = () => {
+  return (
+    <div className='container'>
+      <div className='top-bar'>
+        <input type="text" className="cityInput" placeholder='Search' />
+        <div className='search-icon'>
+          <img src={search_icon} alt=""/>
+        </div>
+      </div>
+      <div className="weather-image">
+        <img src={cloud_icon} alt="" />
+      </div>
+      <div className="weather-temp">24° C</div>
+    </div>
+  )
+}
+
+export default WeatherApp
