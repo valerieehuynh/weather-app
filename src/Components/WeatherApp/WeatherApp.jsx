86d1baa2fff4168042eaa79d1c76a99156755b59
valerieehuynh <vlhuynh2@uci.edu>
2023-09-06 13:15:05 -0700

    create css and containers for app functions

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -7,22 +7,44 @@ import drizzle_icon from '../Assets/drizzle.png';
 import rain_icon from '../Assets/rain.png';
 import snow_icon from '../Assets/snow.png';
 import wind_icon from '../Assets/wind.png';
-import humid_icon from '../Assets/humidity.png';
+import humidity_icon from '../Assets/humidity.png';
 
 
 const WeatherApp = () => {
+  
+  let api_key = "6e7df5599dcb7db50905c2cf48dbe8f3";
+
   return (
     <div className='container'>
       <div className='top-bar'>
         <input type="text" className="cityInput" placeholder='Search' />
         <div className='search-icon'>
-          <img src={search_icon} alt=""/>
+          <img src={search_icon} alt="" className='search-png'/>
         </div>
       </div>
       <div className="weather-image">
         <img src={cloud_icon} alt="" />
       </div>
       <div className="weather-temp">24° C</div>
+      <div className="weather-location">London</div>
+      <div className="data-container">
+        <div className="element">
+          <img src={humidity_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percentage">64%</div>
+            <div className="text">Humidity</div>
+        
+          </div>
+        </div>
+        <div className="element">
+          <img src={wind_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percentage">18 km/hr</div>
+            <div className="text">Wind Speed</div>
+        
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
